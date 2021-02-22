3c5ccfabf5bc10b4d006b1d8a3020fd6f7abb63e
Morenikeji Bello <caeserselassie@gmail.com>
2021-02-22 20:52:49 +0100

    Fix code review corrections

--- bin/main.rb
+++ bin/main.rb
@@ -11,24 +11,50 @@ player2 = gets.chomp
 # Create players objects
 # player1_obj = Player.new(player1, "X" )
 # player2_obj = Player.new(player2, "O" )
-# game begins
+
+# GAME BEGINS
+
 # Game board [1-9]
 # game_board = GameBoard.new()
-# display initial board game board
+
+# DISPLAY INITIAL GAME BOARD
 # game_board.display()
 # inital game board
 #-----1-----|------2-----|-----3-----
 #-----4-----|------5-----|-----6-----
 #-----7-----|------8-----|-----9-----
-plays = 1
-while plays <= 9
+
+in_progress = true
+
+while in_progress
 
   puts
 
   puts "Player 1 #{player1} Select position from  1 - 9"
   position = gets.chomp
 
+  # CHECK FOR INVALID INPUT
+  invalid = true
+
+  puts 'You must enter between values 1 to 9' if invalid
+
+  # CHECK IF POSITION IS TAKEN
+  position_valid = true
+
+  puts 'POSITION ALREADY TAKEN! SELECT AVAILABLE POSITION.' if position_valid
+
   # player1_obj.play(position)
+
+  # CHECK IF WINNING MOVE
+  winning_move = false
+
+  puts 'GAME OVER! PLAYER 1 WINS!' if winning_move
+
+  # CHECK IF GAME IS A TIE
+  is_a_tie = false
+
+  puts 'TOUGH GAME! IT IS A DRAW' if is_a_tie
+
   # board.display()
   puts "Player 1 #{player1} selected this position #{position}"
   puts '-----X-----|------2-----|-----3-----'
@@ -36,28 +62,19 @@ while plays <= 9
   puts '-----7-----|------8-----|-----9-----'
   puts
   puts
-  # Check for wining combinations
-  # break if player wins
-  break if plays == 9
 
-  plays += 1
-
-  puts "Game play: #{plays}"
   puts "Player 2 #{player2} Select position from  1 - 9"
   position = gets.chomp
   puts
   puts "Player 2 #{player2} selected this position #{position}"
+
   # player2_obj.play(position)
   # board.display()
   puts '-----X-----|------2-----|-----3-----'
   puts '-----4-----|------O-----|-----6-----'
   puts '-----7-----|------8-----|-----9-----'
-  # Check for wining combinations
-  # break if player wins
-  break if plays == 9
 
-  puts
-  plays += 1
-  puts "Game play: #{plays}"
+  in_progress = false
+  break if in_progress == false
 
 end
