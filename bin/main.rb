2bacb477f991746920b8eb7f2fe7adae371eda34
Morenikeji Bello <caeserselassie@gmail.com>
2021-02-23 16:01:44 +0100

    Fix rubocop errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,6 @@
-#!/usr/bin/env ruby
+# rubocop:disable Metrics/BlockLength
+
+# !/usr/bin/env ruby
 
 require './lib/Player'
 require './lib/Board'
@@ -28,69 +30,62 @@ player1 = Player.new(player1_name, 'X', board)
 player2 = Player.new(player2_name, 'O', board)
 
 # DISPLAY INITIAL BOARD
-board.display()
+board.display
 
 loop do
-        # PLAYER 1 TAKES TURN
-    puts
+  # PLAYER 1 TAKES TURN
+  puts
+  print "PICK A POSITION, #{player1.name.upcase}: "
+  position = gets.chomp.to_i
+
+  # CHECK IF POSITION INPUT IS BETWEEN 1 TO 9
+  until (1..9).to_a.any?(position)
+    puts 'You can only choose between positions 1 to 9.'
     print "PICK A POSITION, #{player1.name.upcase}: "
     position = gets.chomp.to_i
+  end
 
-    # CHECK IF POSITION INPUT IS BETWEEN 1 TO 9
-    until (1..9).to_a.any?(position)
-        puts "You can only choose between positions 1 to 9."
-        print "PICK A POSITION, #{player1.name.upcase}: "
-        position = gets.chomp.to_i
-    end
+  # CHECK IF POSITION IS TAKEN
+  until board.position_not_used?(position)
+    puts 'Position already taken! Choose a different position.'
+    print "PICK A POSITION, #{player1.name.upcase}: "
+    position = gets.chomp.to_i
+  end
 
-    # CHECK IF POSITION IS TAKEN
-    until board.position_not_used?(position)
-        puts "Position already taken! Choose a different position."
-        print "PICK A POSITION, #{player1.name.upcase}: "
-        position = gets.chomp.to_i
-    end
+  # PLAYER ONE MOVE UPDATES ON BOARD
+  player1.play(position)
 
-    # PLAYER ONE MOVE UPDATES ON BOARD
-    player1.play(position)
+  # CHECK IF WINNING COMBO
+  abort("GAME OVER! #{player1.name.upcase} WINS GAME!") if board.is_winning_combo?(player1.token)
 
-    #CHECK IF WINNING COMBO
-    if board.is_winning_combo?(player1.token)
-        abort("GAME OVER! #{player1.name.upcase} WINS GAME!")
-    end
+  # CHECK IF STALEMATE
+  abort('TOUGH GAME! THERE WAS NO WINNER') if board.is_stalemate?
 
-    # CHECK IF STALEMATE
-    if board.is_stalemate?
-        abort("TOUGH GAME! THERE WAS NO WINNER")
-    end
+  # PLAYER 2 TAKES TURN
+  puts
+  print "PICK A POSITION, #{player2.name.upcase}: "
+  position = gets.chomp.to_i
 
-    # PLAYER 2 TAKES TURN
-    puts
+  # CHECK IF POSITION INPUT IS BETWEEN 1 TO 9
+  until (1..9).to_a.any?(position)
+    puts 'You can only choose between positions 1 to 9.'
+    print "PICK A POSITION, #{player2_name.name.upcase}: "
+    position = gets.chomp.to_i
+  end
+
+  # CHECK IF POSITION IS TAKEN
+  until board.position_not_used?(position)
+    puts 'Position already taken! Choose a different position.'
     print "PICK A POSITION, #{player2.name.upcase}: "
     position = gets.chomp.to_i
+  end
 
-    # CHECK IF POSITION INPUT IS BETWEEN 1 TO 9
-    until (1..9).to_a.any?(position)
-        puts "You can only choose between positions 1 to 9."
-        print "PICK A POSITION, #{player2_name.name.upcase}: "
-        position = gets.chomp.to_i
-    end
+  player2.play(position)
 
-    # CHECK IF POSITION IS TAKEN
-    until board.position_not_used?(position)
-        puts "Position already taken! Choose a different position."
-        print "PICK A POSITION, #{player2.name.upcase}: "
-        position = gets.chomp.to_i
-    end
+  # CHECK IF WINNING COMBO
+  abort("GAME OVER! #{player2.name.upcase} WINS GAME!") if board.is_winning_combo?(player2.token)
 
-    player2.play(position)
-
-    #CHECK IF WINNING COMBO
-    if board.is_winning_combo?(player2.token)
-        abort("GAME OVER! #{player2.name.upcase} WINS GAME!")
-    end
-
-    # CHECK IF STALEMATE
-    if board.is_stalemate?
-        abort("TOUGH GAME! THERE WAS NO WINNER")
-    end
+  # CHECK IF STALEMATE
+  abort('TOUGH GAME! THERE WAS NO WINNER') if board.is_stalemate?
 end
+# rubocop:enable Metrics/BlockLength
