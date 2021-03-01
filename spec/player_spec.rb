cb98c26a274ad7d6cb7d465b2c94a42a0ffcf874
Morenikeji Bello <caeserselassie@gmail.com>
2021-03-01 16:30:17 +0100

    Add test logic for play method

---
+++ spec/player_spec.rb
@@ -0,0 +1,12 @@
+require './lib/player.rb'
+require './lib/board.rb'
+
+describe Player do
+  describe '#play' do
+    it 'sets chosen position to player token' do
+      board = Board.new
+      player = Player.new('Rashad', 'O', board)
+      expect(player.play(2)).to eql('O')   
+    end
+  end
+end
